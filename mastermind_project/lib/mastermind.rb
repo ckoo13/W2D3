2c3f2241f9a0df1c42f7a5917daa8a8916fb3f23
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-06-05 21:46:56 -0400

    Mastermind Complete

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,26 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
 
+    def print_matches(instance)
+        p instance.num_near_matches(@secret_code)
+        p instance.num_exact_matches(@secret_code)
+    end
+
+    def ask_user_for_guess
+        p "Enter a code"
+
+        input = gets.chomp
+
+        guess = Code.from_string(input)
+
+        p guess.num_exact_matches(@secret_code)
+        p guess.num_near_matches(@secret_code)
+
+        return guess == @secret_code
+
+    end
 end
